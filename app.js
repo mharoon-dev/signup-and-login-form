d0fbfe9d8721af8617bce4cb7822ca9de6c13bbc
mharoon-dev <haroon.aghani@gmail.com>
2023-11-26 19:04:20 +0500

    saveing data in localStorage

--- app.js
+++ app.js
@@ -1,34 +1,72 @@
-let name = document.querySelector('#name')
+let name = document.querySelector('#name');
 let email = document.querySelector('#email')
-let password = document.querySelector('#password')
-let conformPassword = document.querySelector('#conformPassword')
+let password = document.querySelector('#password');
+let conformPassword = document.querySelector('#conformPassword');
 let submit = document.querySelector('#submit')
 
-let namesArray = []
-let emailArray = []
-let passwordArray = []
-let conformPasswordArray = []
+
+let storedUserInfo = localStorage.getItem("userinfo");
+let userInfo = storedUserInfo ? JSON.parse(storedUserInfo) : [];
+
+// submit.addEventListener("click", () => {
+//     if (name.value == '' || email.value == ''  || password.value == ''  || conformPassword.value == '') {
+//         alert('please fill the form  completely!')
+//     } else if (password.value !== conformPassword.value)  {
+//         alert("password and conform password are not equal!")
+//     } else {
+//         let user = {
+//             name:name.value,
+//             email:email.value,
+//             password:password.value,
+//             conformPassword:conformPassword.value
+//         }
+
+//         // userInfo.push(user)
+//         localStorage.setItem("userinfo", JSON.stringify(userInfo));
+//         console.log(userInfo);
+
+
+        
+
+//         name.value = ''
+//         email.value = ''
+//         password.value = ''
+//         conformPassword.value = ''
+//     }
+
+// })
+
+
 
 submit.addEventListener("click", () => {
-    if (name.value == '' || email.value == ''  || password.value == ''  || conformPassword.value == '') {
-        alert('please fill the form  completely!')
-    } else if (password.value !== conformPassword.value)  {
-        alert("password and conform password are not equal!")
+    if (name.value == '' || email.value == '' || password.value == '' || conformPassword.value == '') {
+        alert('Please fill the form completely!');
+    } else if (password.value !== conformPassword.value) {
+        alert("Password and confirm password are not equal!");
     } else {
-        console.log("done");
+        let user = {
+            name: name.value,
+            email: email.value,
+            password: password.value,
+            conformPassword: conformPassword.value
+        };
+
+        // Push the new user data into the array
+        userInfo.push(user);
+
+        // Update the stored user information in localStorage
+        localStorage.setItem("userinfo", JSON.stringify(userInfo));
+
+        console.log(userInfo);
+
+        // Clear the input fields
+        name.value = '';
+        email.value = '';
+        password.value = '';
+        conformPassword.value = '';
     }
+});
 
-
-    namesArray.unshift(name.value)
-    emailArray.unshift(email.value)
-    passwordArray.unshift(password.value)
-    conformPasswordArray.unshift(conformPassword.value)
-    console.log(namesArray);
-    console.log(emailArray);
-    console.log(passwordArray);
-    console.log(conformPasswordArray);
-
-})
 
 
 
