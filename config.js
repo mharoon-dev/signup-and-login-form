409bbad41fc03f320c7b56237d99e5465f590f58
mharoon-dev <haroon.aghani@gmail.com>
2023-12-27 18:01:45 +0500

    signup with fire base

---
+++ config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth , createUserWithEmailAndPassword  } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDNFD6gaEs86-2ONnXdo2XtfJnOYUE1Dww",
+  authDomain: "login-and-sign-up-2bf4c.firebaseapp.com",
+  projectId: "login-and-sign-up-2bf4c",
+  storageBucket: "login-and-sign-up-2bf4c.appspot.com",
+  messagingSenderId: "509642645813",
+  appId: "1:509642645813:web:3c21795714a1db86b4ec7c",
+  measurementId: "G-KB5LDS5G2N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app , auth , createUserWithEmailAndPassword  }
--- sign up/app.js
+++ sign up/app.js
@@ -1,56 +1,90 @@
-let signUpBox = document.querySelector('.signUp-box');
-let name = document.querySelector('#name');
-let email = document.querySelector('#email');
-let password = document.querySelector('#password');
-let conformPassword = document.querySelector('#conformPassword');
-let submit = document.querySelector('#submit');
-let loader = document.querySelector('.loader');
+// let signUpBox = document.querySelector('.signUp-box');
+// let name = document.querySelector('#name');
+// let email = document.querySelector('#email');
+// let password = document.querySelector('#password');
+// let conformPassword = document.querySelector('#conformPassword');
+// let submit = document.querySelector('#submit');
+// let loader = document.querySelector('.loader');
 
-let getingDataFromLocalStorage = JSON.parse(localStorage.getItem('userInfo')) || [];
-let userInfo = getingDataFromLocalStorage;
+// let getingDataFromLocalStorage = JSON.parse(localStorage.getItem('userInfo')) || [];
+// let userInfo = getingDataFromLocalStorage;
 
-submit.addEventListener('click', () => {
-  // userInput empty condition
-  if (name.value === '' || email.value === '' || password.value === '' || conformPassword.value === '') {
-    alert('Please fill the form completely!');
-  } else if (password.value !== conformPassword.value) {     // password and conform password matching
-    alert('Password and confirm password are not equal!');
-  } else { 
-    // checking user name and email
-    let userAlreadyTaken = userInfo.find((user) => {
-      return name.value === user.name || email.value === user.email;
+// submit.addEventListener('click', () => {
+//   // userInput empty condition
+//   if (name.value === '' || email.value === '' || password.value === '' || conformPassword.value === '') {
+//     alert('Please fill the form completely!');
+//   } else if (password.value !== conformPassword.value) {     // password and conform password matching
+//     alert('Password and confirm password are not equal!');
+//   } else { 
+//     // checking user name and email
+//     let userAlreadyTaken = userInfo.find((user) => {
+//       return name.value === user.name || email.value === user.email;
+//     });
+
+//     if (!userAlreadyTaken) {
+//       let user = {
+//         name: name.value,
+//         email: email.value,
+//         password: password.value,
+//         conformPassword: conformPassword.value,
+//       };
+
+//       userInfo.push(user);
+//       localStorage.setItem('userInfo', JSON.stringify(userInfo));
+//       console.log(userInfo);
+
+//       name.value = '';
+//       email.value = '';
+//       password.value = '';
+//       conformPassword.value = '';
+
+//       signUpBox.style.display = "none"
+//       loader.style.display = "block"
+
+//     //   window location changing
+//       setTimeout(() => {
+//         window.location.href = '../login/login.html';
+//       }, 3000);
+//     } else {
+//       alert('Sorry, this username or email is already taken!');
+//     }
+//   }
+// });
+
+
+
+
+
+// sign up with fire base
+// Importing necessary values from "../config.js"
+import { app, auth, createUserWithEmailAndPassword } from "../config.js";
+
+// Sign up handler function
+window.signUpHandler = () => {
+  const email = document.querySelector('#email');
+  const password = document.querySelector('#password');
+  
+  console.log(email.value);
+  console.log(password.value);
+
+  // Firebase functionality
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      alert("Sign up successfully!");
+      window.location = "../login/login.html";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(errorCode, errorMessage);
     });
+};
 
-    if (!userAlreadyTaken) {
-      let user = {
-        name: name.value,
-        email: email.value,
-        password: password.value,
-        conformPassword: conformPassword.value,
-      };
-
-      userInfo.push(user);
-      localStorage.setItem('userInfo', JSON.stringify(userInfo));
-      console.log(userInfo);
-
-      name.value = '';
-      email.value = '';
-      password.value = '';
-      conformPassword.value = '';
-
-      signUpBox.style.display = "none"
-      loader.style.display = "block"
-
-    //   window location changing
-      setTimeout(() => {
-        window.location.href = '../login/login.html';
-      }, 3000);
-    } else {
-      alert('Sorry, this username or email is already taken!');
-    }
-  }
-});
-
+// Adding click event listener to the submit button
+const submit = document.querySelector('#submit');
+submit.addEventListener('click', signUpHandler);
 
                 
                 
